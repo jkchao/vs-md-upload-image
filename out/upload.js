55fa62584b15dab1d979b63f9b3225f8bca0d6f6
jkchao <jkchaom@gmail.com>
2019-04-23 19:33:00 +0800

    remove: out

--- out/upload.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const vscode_1 = require("vscode");
-function uploadFile(path) {
-    const config = vscode_1.workspace.getConfiguration('qiniu');
-}
-exports.uploadFile = uploadFile;
-//# sourceMappingURL=upload.js.map
