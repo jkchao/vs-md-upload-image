17d43ca85a351224b93dc83356e14ffbe9f97dbf
jkchao <jkchaom@gmail.com>
2019-04-24 21:59:12 +0800

    feat: support view image before upload

--- src/selectFileToUpload.ts
+++ src/selectFileToUpload.ts
@@ -1,7 +1,9 @@
 import { hasConfig, checkCurrentFile } from './utils';
-import { window, ViewColumn } from 'vscode';
+import { window, ViewColumn, Uri } from 'vscode';
 import { insertToMd } from './insert';
 import { createWebviewContent } from './webviewContent';
+import { writeFileSync, readFileSync, createReadStream, createWriteStream, fstat, mkdirSync, existsSync } from 'fs';
+import { join, basename } from 'path';
 
 export function selectFileToUpload() {
   if (!hasConfig() || !checkCurrentFile()) {
@@ -14,11 +16,21 @@ export function selectFileToUpload() {
     })
     .then(file => {
       if (!file) return;
-      const { path } = file[0];
+      const { path: filePath } = file[0];
+
+      const path = join(__dirname, '..', `/images/`);
+
+      if (!existsSync(path)) {
+        mkdirSync(path);
+      }
+
+      const outPut = `${path}${basename(filePath)}`;
+
+      writeFileSync(`${outPut}`, readFileSync(filePath));
 
       const paner = window.createWebviewPanel('image Corp', 'Image Corp', ViewColumn.One);
 
-      paner.webview.html = createWebviewContent(path);
+      paner.webview.html = createWebviewContent(outPut);
       // return insertToMd(path);
     });
 }
