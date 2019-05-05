52b1c7448c1342743199f4c4bfeb68c8961b4e61
jkchao <jkchaom@gmail.com>
2019-05-05 16:08:05 +0800

    feat: add image feat

--- src/component/Crop/index.tsx
+++ src/component/Crop/index.tsx
@@ -3,6 +3,7 @@ import './index.scss';
 
 interface State {
   isActive: boolean;
+  src: string;
   style: {
     top: string;
     left: string;
@@ -20,42 +21,71 @@ export class Crop extends React.PureComponent<{}, State> {
   containerRef: HTMLDivElement | null = null;
   imageRef: HTMLImageElement | null = null;
 
-  state = {
-    isActive: false,
-    style: {
-      top: '100px',
-      left: '100px',
-      width: '200px',
-      height: '200px'
-    }
-  };
-
   corpData = {
     startX: 0,
     startY: 0,
     offsetX: 0,
     offsetY: 0,
-    imageWidth: 0,
-    imageHeight: 0,
+
     cropWidth: 0,
-    cropHeight: 0
+    cropHeight: 0,
+    isResize: false,
+    imageData: {
+      imageWidth: 0,
+      imageHeight: 0,
+      imageLeft: 0,
+      imageTop: 0
+    }
   };
 
+
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      isActive: false,
+      src: '',
+      style: {
+        top: '0',
+        left: '0',
+        width: '0',
+        height: '0'
+      }
+    };
+  }
+
   componentDidMount() {
+    // vscode message
+    window.addEventListener('message', this.onMessage, false);
     window.addEventListener('mousemove', this.onMouseMove, false);
     window.addEventListener('mouseup', this.onMouseUp, false);
   }
 
   componentWillUnmount() {
+    window.removeEventListener('message', this.onMessage);
     window.removeEventListener('mousemove', this.onMouseMove);
     window.removeEventListener('mouseup', this.onMouseUp);
+  }
+
+  onMessage = (e: MessageEvent) => {
+    const message = e.data;
+
+    console.log(e);
+
+    switch(message.command) {
+      case 'image':
+        this.setState({
+          src: message.data
+        });
+        break;
+    }
   }
 
   getPos = (e: MouseEvent) => {
     const { pageX, pageY } = e;
     const {
       offsetX, startX, offsetY, startY,
-      imageWidth, imageHeight, cropWidth, cropHeight
+      imageData: { imageWidth, imageHeight },
+      cropWidth, cropHeight
      } = this.corpData;
 
     const diffY = pageY - startY;
@@ -73,16 +103,94 @@ export class Crop extends React.PureComponent<{}, State> {
 
     if(!this.state.isActive) return;
 
-    const { top, left } = this.getPos(e);
+    // 调整大小
+    if (this.corpData.isResize) {
+      const {
+        startX, startY,
+        imageData: { imageLeft, imageTop, imageWidth, imageHeight },
+        offsetX, offsetY
+      } = this.corpData;
+      let width = 0;
+      let height = 0;
+      let left = 0;
+      let top = 0;
 
-    this.setState({
-      style: {
-        width: '200px',
-        height: '200px',
-        top: top + 'px',
-        left: left + 'px'
+      if (
+        e.pageX < startX ||
+        e.pageY < startY
+        ) {
+          width = Math.abs(e.pageX - startX);
+          height = Math.abs(e.pageY - startY);
+
+          if (e.pageX < startX && e.pageY > startY) {
+            top = startY - imageTop;
+            left = e.pageX - imageLeft;
+
+          } else if (e.pageX > startX && e.pageY < startY) {
+
+            top = e.pageY - imageTop;
+            left = startX - imageLeft;
+
+          } else {
+            top = e.pageY - imageTop;
+            left = e.pageX - imageLeft;
+          }
+
+      } else {
+        width = e.pageX - startX;
+        height = e.pageY - startY;
+        top = this.corpData.offsetY;
+        left = this.corpData.offsetX;
       }
-    });
+
+
+      // 左边界
+      if (left < 0) {
+        width = startX - imageLeft;
+        left = 0;
+      }
+
+      // 上边界
+      if (top < 0) {
+        height = startY - imageTop;
+        top = 0;
+      }
+
+      // 右边界
+      if (e.pageX > startX && width + offsetX > imageWidth) {
+        width = imageWidth - offsetX - 1;
+      }
+
+      // 下边界
+      if (e.pageY > startY && height + offsetY > imageHeight) {
+        height = imageHeight - offsetY - 1;
+      }
+
+      this.setState({
+        style: {
+          width: width + 'px',
+          height: height + 'px',
+          top: top + 'px',
+          left: left + 'px'
+        }
+      });
+
+      this.corpData = {
+        ...this.corpData,
+        cropWidth: width,
+        cropHeight: height
+      };
+    } else {
+      // 只有拖拽移动
+      const { top, left } = this.getPos(e);
+      this.setState({
+        style: {
+          ...this.state.style,
+          top: top + 'px',
+          left: left + 'px'
+        }
+      });
+    }
   }
 
   onMouseUp = (e: MouseEvent) => {
@@ -118,10 +226,49 @@ export class Crop extends React.PureComponent<{}, State> {
       startY: e.pageY,
       offsetX: left - imageLeft,
       offsetY: top - imageTop,
-      imageWidth,
-      imageHeight,
+      imageData: {
+        imageLeft,
+        imageTop,
+        imageWidth,
+        imageHeight
+      },
       cropWidth,
-      cropHeight
+      cropHeight,
+      isResize: false
+    };
+
+    this.setState({
+      isActive: true
+    });
+  }
+
+  containerMouseDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+
+    if (e.target !== this.imageRef) return;
+
+    e.preventDefault();
+
+    const {
+      width: imageWidth,
+      height: imageHeight,
+      left: imageLeft,
+      top: imageTop
+    } = this.imageRef!.getBoundingClientRect();
+
+    this.corpData = {
+      startX: e.pageX,
+      startY: e.pageY,
+      offsetX: e.pageX - imageLeft,
+      offsetY: e.pageY - imageTop,
+      imageData: {
+        imageLeft,
+        imageTop,
+        imageWidth,
+        imageHeight
+      },
+      cropWidth: 0,
+      cropHeight: 0,
+      isResize: true
     };
 
     this.setState({
@@ -131,27 +278,30 @@ export class Crop extends React.PureComponent<{}, State> {
 
   public render() {
 
-    const { style } = this.state;
+    const { style, src } = this.state;
+
+    const { cropWidth, cropHeight } = this.corpData;
 
     return (
       <div
         className="container"
-        ref={n => this.containerRef = n}>
+        ref={n => this.containerRef = n}
+        onMouseDown={this.containerMouseDown}>
         <img
-          src="https://static.jkchao.cn/TypeScript.png"
+          src={src}
           ref={n => this.imageRef = n}/>
-        <div
+        {cropWidth && cropHeight && <div
           className="selection"
           onMouseDown={this.onMouseDown}
           ref={n => this.sectionRef = n}
           style={style}>
           <div className="elements">
-            <div className="handle nw"></div>
-            <div className="handle ne"></div>
-            <div className="handle se"></div>
-            <div className="handle sw"></div>
+            <div className="handle nw" data-ord="nw"></div>
+            <div className="handle ne" data-ord="ne"></div>
+            <div className="handle se" data-ord="se"></div>
+            <div className="handle sw" data-ord="sw"></div>
           </div>
-        </div>
+        </div>}
       </div>
     );
   }
